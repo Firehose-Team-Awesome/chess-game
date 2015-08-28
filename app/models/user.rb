fd2cb1d82b4f581f66b04e699d11bd3c03da8a31
George Welsted <george@gwelsted.com>
2015-08-28 15:21:20 +0000

    fixed some assoc issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
-	has_many :games
+	has_many :games_as_white_uid, :foreign_key =>'white_uid' , :class_name => 'Game'
+	has_many :games_as_black_uid, :foreign_key =>'black_uid' , :class_name => 'Game'
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def games
+  	games_as_white_uid + games_as_black_uid
+  end
+
+
 end
