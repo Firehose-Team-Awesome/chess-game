185c4482525488f560ec77a9d40de6cc4f5fadbb
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-09-08 23:15:27 +0000

    update valid_move to check board coordinates, that the piece is moved to a diff location, & removed check to see if a location is occupied.

--- app/models/king.rb
+++ app/models/king.rb
@@ -5,18 +5,21 @@ class King < Piece
 		start_x, start_y = start_point
 		end_x, end_y = end_point
 
+		#check that the start and end points are on the board
+		return false if start_x < 0 || start_x > 7
+		return false if start_y < 0 || start_y > 7
+		return false if end_x < 0 || end_x > 7
+		return false if end_y < 0 || end_y > 7
+
+		#check to ensure the piece is not moved to the same location
+		return false if start_point == end_point
+
 		#determine if move is only 1 space in any direction
-		if (start_x - end_x).abs <= 1 && (start_y - end_y).abs <= 1
-			#then check if space is occupied
-			(game.pieces.where(pos_x: end_x, pos_y: end_y, active: true).present?) ? false : true
+		return (start_x - end_x).abs <= 1 && (start_y - end_y).abs <= 1
 			
-			#once movement for other pieces is sorted out, this method will also have to check
-			#if the space the king is moving into will put it in check, which will also be an
-			#invalid move
-		end
-
-
-
+		#once movement for other pieces is sorted out, this method will also have to check
+		#if the space the king is moving into will put it in check, which will also be an
+		#invalid move
 	end
 
 	
