3e82e9cfa5b1e5eb7357b347c864883003df0a6b
George Welsted <george@gwelsted.com>
2015-09-09 00:55:26 +0000

    show all pieces on board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,8 @@
 class GamesController < ApplicationController
+	def show
+    @game = Game.find(params[:id])
+    @pieces = @game.pieces
+    @black_player = User.find_by(id: @game.black_uid)
+    @white_player = User.find_by(id: @game.white_uid)
+  end
 end
