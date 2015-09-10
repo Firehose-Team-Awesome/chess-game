8be80803f3768b49dce6d7f0c1499d0a2277b218
Jeff Powers <jeffp2007@gmail.com>
2015-09-10 04:38:04 +0000

    Support joining a game. Need to update black_uid attribute field to current_user.id once we add authentication.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,26 @@
 class GamesController < ApplicationController
+
+	def index
+		@games = Game.all
+		@open_games = Game.where(:black_uid => nil)
+	end
+
+	def edit
+		@game = Game.find(params[:id])
+	end
+
+	def update
+		@game = Game.find(params[:id])
+		@game.update_attributes(game_params)
+		flash[:notice] = "You successfully joined the game!"
+		redirect_to game_path(@game)
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:black_uid)
+	end
+
+
 end
