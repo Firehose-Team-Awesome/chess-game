9fdc7b23e1d769e1729b4245d4a8586e653a9ed1
Jeff Powers <jeffp2007@gmail.com>
2015-09-15 02:50:40 +0000

    Fixed tests, made simpler, tested more cases

--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -2,45 +2,52 @@ require 'test_helper'
 
 class RookTest < ActiveSupport::TestCase	
 	##### TEST VALID_MOVE METHOD ######
+	# --> clear test db --> rake db:test:prepare
 
   test "check if move is valid (invalid case, no movement)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([0, 0])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, off board)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([3, -1])
 		assert_not move	
 	end
 
-	test "check if move is valid (valid case, horizontal move)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+	test "check if move is valid (valid case, left horizontal move)" do
+		rook = Rook.create(:pos_x => 7, :pos_y => 0)
+		move = rook.valid_move?([1, 0])
+		assert move	
+	end
+
+	test "check if move is valid (valid case, right horizontal move)" do
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([5, 0])
 		assert move	
 	end
 
-	test "check if move is valid (valid case, vertical move)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+	test "check if move is valid (valid case, up vertical move)" do
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([0, 5])
 		assert move	
 	end
 
+	test "check if move is valid (valid case, down vertical move)" do
+		rook = Rook.create(:pos_x => 0, :pos_y => 7)
+		move = rook.valid_move?([0, 4])
+		assert move	
+	end
+
 	test "check if move is valid (invalid case, diagonal move)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([1, 1])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, big jump move)" do
-		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+		rook = Rook.create(:pos_x => 0, :pos_y => 0)
 		move = rook.valid_move?([3, 6])
 		assert_not move	
 	end
