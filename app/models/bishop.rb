51165b496adfc3b674e776218857b7335432e43c
Jeff Powers <jeffp2007@gmail.com>
2015-09-23 01:33:48 +0000

    Fixed tests and added obstruction validation

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -2,12 +2,16 @@ class Bishop < Piece
 	def valid_move?(end_point)
 		# Define ending coordinates for bishop
 		end_x, end_y = end_point
+		start_point = self.current_pos
 
 		#check that the start and end points are on the board
 		return false unless self.on_board?(end_point)
 		
 		#check to ensure the piece is not moved to the same location
 		return false unless self.new_point?(end_point)
+
+		#check for obstructions in path
+		return false if self.is_obstructed?(start_point, end_point)
 
 		#determine if move is diagonal
 		(-7..7).each do |i|
--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -6,56 +6,72 @@ class BishopTest < ActiveSupport::TestCase
 	#---> to reset the database --> rake db:test:prepare
 
   test "check if move is valid (invalid case, no movement)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
+  	game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = game.pieces.where(:pos_x => 2).where(:pos_y => 0).first
 		move = bishop.valid_move?([2, 0])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, off board)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
-		move = bishop.valid_move?([3, -1])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = game.pieces.where(:pos_x => 2).where(:pos_y => 0).first
+		move = bishop.valid_move?([2, -1])
+		assert_not move	
+	end
+
+	test "check if move is valid (invalid case, obstruction)" do
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = game.pieces.where(:pos_x => 2).where(:pos_y => 0).first
+		move = bishop.valid_move?([4, 2])
 		assert_not move	
 	end
 
 	test "check if move is valid (valid case, quadrant 1 diagonal move)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
-		move = bishop.valid_move?([6, 4])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
+		move = bishop.valid_move?([2, 4])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, quadrant 2 diagonal move)" do
-		bishop = Bishop.create(:pos_x => 0, :pos_y => 6)
-		move = bishop.valid_move?([5, 1])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 0, :pos_y => 4, :game_id => game.id)
+		move = bishop.valid_move?([2, 2])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, quadrant 3 diagonal move)" do
-		bishop = Bishop.create(:pos_x => 6, :pos_y => 7)
-		move = bishop.valid_move?([1, 2])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 2, :pos_y => 4, :game_id => game.id)
+		move = bishop.valid_move?([0, 2])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, quadrant 4 diagonal move)" do
-		bishop = Bishop.create(:pos_x => 6, :pos_y => 0)
-		move = bishop.valid_move?([1, 5])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 2, :pos_y => 2, :game_id => game.id)
+		move = bishop.valid_move?([0, 4])
 		assert move	
 	end
 
 	test "check if move is valid (invalid case, move horizontally)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
-		move = bishop.valid_move?([6, 0])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
+		move = bishop.valid_move?([2, 2])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, move vertically)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
-		move = bishop.valid_move?([2, 5])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
+		move = bishop.valid_move?([0, 4])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, big jump)" do
-		bishop = Bishop.create(:pos_x => 2, :pos_y => 0)
-		move = bishop.valid_move?([5, 6])
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		bishop = Bishop.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
+		move = bishop.valid_move?([4, 5])
 		assert_not move	
 	end
 end
