b757d1e415ee80e987b93167baa15c3e0dad1e7e
Jeff Powers <jeffp2007@gmail.com>
2015-09-23 01:12:00 +0000

    Fixed tests and added obstruction validation

--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -6,56 +6,63 @@ class RookTest < ActiveSupport::TestCase
 
   test "check if move is valid (invalid case, no movement)" do
   	game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0, :pos_y => 0)
+		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
 		move = rook.valid_move?([0, 0])
 		assert_not move
 	end
 
 	test "check if move is valid (invalid case, off board)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = game.pieces.where(:pos_x => 0, :pos_y => 0)
+		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
 		move = rook.valid_move?([0, -1])
+		assert_not move	
+	end
+
+	test "check if move is valid (invalid case, obstruction)" do
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		rook = game.pieces.where(:pos_x => 0).where(:pos_y => 0).first
+		move = rook.valid_move?([0, 2])
 		assert_not move	
 	end
 
 	test "check if move is valid (valid case, left horizontal move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 1, :pos_y => 2)
+		rook = Rook.create(:pos_x => 1, :pos_y => 2, :game_id => game.id)
 		move = rook.valid_move?([0, 2])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, right horizontal move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 0, :pos_y => 2)
+		rook = Rook.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
 		move = rook.valid_move?([1, 2])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, up vertical move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 0, :pos_y => 2)
+		rook = Rook.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
 		move = rook.valid_move?([0, 3])
 		assert move	
 	end
 
 	test "check if move is valid (valid case, down vertical move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 0, :pos_y => 3)
+		rook = Rook.create(:pos_x => 0, :pos_y => 3, :game_id => game.id)
 		move = rook.valid_move?([0, 2])
 		assert move	
 	end
 
 	test "check if move is valid (invalid case, diagonal move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 0, :pos_y => 2)
+		rook = Rook.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
 		move = rook.valid_move?([1, 3])
 		assert_not move	
 	end
 
 	test "check if move is valid (invalid case, big jump move)" do
 		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
-		rook = Rook.create(:pos_x => 0, :pos_y => 2)
+		rook = Rook.create(:pos_x => 0, :pos_y => 2, :game_id => game.id)
 		move = rook.valid_move?([3, 4])
 		assert_not move	
 	end
