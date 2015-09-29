fec613c1a9968eca87d250fa26458329805ab2ba
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-09-29 01:19:35 +0000

    added test for can_capture_en_passant, not working properly currently

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -11,30 +11,12 @@ class Pawn < Piece
 
 	#determine if pawn can capture opposing pawn en passant
 	def can_capture_en_passant(opponent_pawn)
-		if opponent_pawn.en_passant == true && opponent_pawn.updated_at == game.updated_at
+		if opponent_pawn.updated_at == game.updated_at && opponent_pawn.en_passant == true 
 			return true
 		else
 			return false
 		end
 	end
 
-	#do the capture en passant move
-	def capture_en_passant(opponent_pawn)
-		pos_x = opponent_pawn.pos_x
-		pos_y = opponent_pawn.pos_y
-
-		if can_capture_en_passant(opponent_pawn)
-			#first move to where opponent pawn actually is to take care of capturing the piece
-			self.move_to!(pos_x, pos_y)
-
-			#then move from where the pawn was to one space behind it, where it was theoretically "captured" in passing
-			#determine if captured pawn was on top or bottom side of board
-			if pos_y == 3
-				self.move_to!(pos_x, 2)
-			else
-				self.move_to!(pos_x, 5)
-			end
-		end
-
-	end
+	
 end
--- test/models/pawn_test.rb
+++ test/models/pawn_test.rb
@@ -19,4 +19,17 @@ class PawnTest < ActiveSupport::TestCase
 		assert_not pawn.en_passant	
 	end
 
+	test "can capture en passant (true case)" do
+		game = Game.create(:white_uid => 1, :black_uid => 1, :game_name => "New Game")
+		pawn_black = game.pieces.where(:pos_x => 4).where(:pos_y => 1).first
+		pawn_white = game.pieces.where(:pos_x => 5).where(:pos_y => 6).first			
+		pawn_black.update_en_passant_status(4, 3)		
+		pawn_white.update_attributes(:pos_x => 5, :pos_y => 3)
+		pawn_black.update_attributes(:pos_x => 4, :pos_y => 3)
+		can_capture = pawn_white.can_capture_en_passant(pawn_black)
+		puts pawn_black.updated_at
+		puts game.updated_at
+		assert can_capture
+	end
+
 end
