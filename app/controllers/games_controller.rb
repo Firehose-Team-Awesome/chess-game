5fd4bde1ff78efc5d07687cad813b4e371e2e87b
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-10-05 16:35:54 +0000

    display message on game page if either player is in check

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,13 @@ class GamesController < ApplicationController
     @pieces = @game.pieces
     @black_player = User.find_by(id: @game.black_uid)
     @white_player = User.find_by(id: @game.white_uid)
+    if @game.is_color_in_check?(0) && @game.is_color_in_check?(1)
+      flash[:notice] = "Both players are in check." 
+    elsif @game.is_color_in_check?(0)
+      flash[:notice] = "Black player is in check." 
+    elsif @game.is_color_in_check?(1)
+      flash[:notice] = "White player is in check." 
+    end  
   end
 
 	def edit
